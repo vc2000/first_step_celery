447f97a12401eefa00831fc03155fa693ac538f9
vc2000 <vc2000@wildcats.unh.edu>
2018-07-24 17:14:45 -0400

    need to improve config files

---
+++ learncelery/app/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from celery import Celery
+import celeryconfig 
+import config
+from tasks.test import *
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+def make_celery(app):
+    # create context tasks in celery
+    celery = Celery(
+        app.import_name,
+        broker=app.config['BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    celery.config_from_object(celeryconfig)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+
+    return celery
+
+celery = make_celery(app)
+
+
+@app.route('/')
+def view():
+    return "Hello, Flask is up and running!"
+
+@app.route('/play')
+def get_play():  
+    play_task.delay()
+    return 'Playing! <a href="/">back</a>'
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ learncelery/app/tasks/test.py
@@ -0,0 +1,18 @@
+import celery
+
+
+@celery.task()
+def print_hello():
+    logger = print_hello.get_logger()
+    logger.info("Hello")
+
+
+@celery.task(name='tasks.play_task')
+def play_task():  
+    print('play something')
+    return "say hi"
+
+@celery.task(name='tasks.pause_task')
+def pause_task():  
+    print('enough fun')
+    return 'enough fun'
